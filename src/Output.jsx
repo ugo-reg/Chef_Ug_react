2fa4d7c9a704f9310d5d80e25b6f708d7ca8ee8f
ugo-reg <ugochi209@gmail.com>
2025-07-04 11:33:11 -0700

    debugging

--- src/Output.jsx
+++ src/Output.jsx
@@ -1,26 +1,16 @@
 import ReactMarkdown from 'react-markdown'
 
- function Output(){
+ function Output(props){
  
     return(
         <>
             <section>
     <h2>Chef-UG Recommends:</h2>
-   <p>Instructions:
+    
+    <ReactMarkdown>
+    {props.recipe}
+    </ReactMarkdown>
 
-Rinse the rice under cold water until the water runs clear.
-
-In a pot, bring water to a boil.
-
-Add rice, salt, and butter/oil (if using).
-
-Cover and reduce heat to low.
-
-Simmer for 15–18 minutes (until water is absorbed).
-
-Remove from heat and let sit 5 minutes, covered.
-
-Fluff with a fork and serve.</p>
 </section>
 
         </>
